93d504a5010c63bee3b153e63d3a1c52f4cfda5e
zedddie <rust@zedddie.rs>
2026-03-30 02:11:19 +0200

    flake rust nightly overlay

--- flake.nix
+++ flake.nix
@@ -1,24 +1,26 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
   outputs =
     {
       self,
       nixpkgs,
+      rust-overlay,
     }:
     let
       system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-
-      runtimeLibs = with pkgs; [
-      ];
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ rust-overlay.overlays.default ];
+      };
     in
     {
       devShells.${system}.default = pkgs.mkShell {
         nativeBuildInputs = with pkgs; [
-          cargo
+          rust-bin.nightly.latest.default
           rustfmt
           git
           pkg-config
